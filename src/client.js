e3c30cbbfa42918ba1253b054a7e27eaeeddc9bb
evgeny <afazhenya2001@gmail.com>
2024-01-30 19:04:03 +0300

    routing without refresh

--- src/client.js
+++ src/client.js
@@ -1,1 +1,35 @@
-console.log('!@client is here!')
+let currentPathName = window.location.pathname
+
+const navigate = async (pathName) => {
+    currentPathName = pathName
+    const response = await fetch(pathName)
+    const receivedHTML = await response.text()
+
+    const bodyStartIndex = receivedHTML.indexOf('<body>') + '<body>'.length
+    const bodyEndIndex = receivedHTML.lastIndexOf('</body>')
+    document.body.innerHTML = receivedHTML.slice(bodyStartIndex, bodyEndIndex)
+}
+
+window.addEventListener(
+    'click',
+    (e) => {
+        if (e.target.tagName !== 'A') {
+            return
+        }
+        if (e.metaKey || e.ctrlKey || e.shiftKey || e.altKey) {
+            return
+        }
+        const href = e.target.getAttribute('href')
+        if (!href.startsWith('/')) {
+            return
+        }
+        e.preventDefault()
+        window.history.pushState(null, null, href)
+        navigate(href)
+    },
+    true
+)
+
+window.addEventListener('popstate', () => {
+    navigate(window.location.pathname)
+})
